9fac808c4a8010b3eb5141b664ec5ea64b792c52
tharikhajar <tharikhajar@gmail.com>
2021-05-08 19:19:06 -0300

    test

---
+++ parameters_bsm2.py
@@ -0,0 +1,5 @@
+# This file holds the parameter set from Rosen; Jeppsson 2006 implementation of ADM1 within the BSM2 framework
+# This implementation is intended for urban waste water digestion and is widely used as an implementation validadion
+
+
+
